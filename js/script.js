41e30803907bc38a1b725f649c7298200e5cde3c
Cole <cole.smith.code@gmail.com>
2022-05-30 12:20:20 -0600

    debugging js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // global variables
 let employees = [];
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
-email, location, phone, dob &noinfo &nat=US`
+      email, location, phone, dob &noinfo &nat=US`
 const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
@@ -42,56 +42,42 @@ function displayEmployees(employeeData) {
         gridContainer.innerHTML = employeeHTML;
 }
 
-const displayModal = (index) => {
-    
-    modalContent.innerHTML = ``
-    modalContent.setAttribute("data-index", index)
-  
-    const {
-      name,
-      dob,
-      phone,
-      email,
-      location: { city, street, state, postcode },
-      picture,
-    } = filteredEmployees[index]
-  
-    const birthdayDate = new Date(dob.date)
-  
-    const employeeInfoDiv = document.createElement("div")
-  
-    employeeInfoDiv.innerHTML = `
-      <img class="avatar" src="${picture.large}" />
-      <div class="text-container">
-        <h2 class="name">${name.first} ${name.last}</h2>
-        <p class="email">${email}</p>
-        <p class="address">${city}</p>
-        <hr />
-        <p>${phone}</p>
-        <p class="address">${street.number} ${
-      street.name
-    }, ${city}, ${state}, ${postcode}</p>
-        <p>Birthday: ${
-          birthdayDate.getMonth() + 1
-        }/${birthdayDate.getDate()}/${birthdayDate.getFullYear()}</p>
-      </div>
-    `
-  
-    modalContent.append(employeeInfoDiv)
-    overlay.classList.remove("hidden")
-    modalClose.addEventListener("click", () => removeModal(employeeInfoDiv))
-}
-  
-  /**
-   * Close the employee modal overlay
-   * @param {object} employeeInfoDiv
-   */
-  const removeModal = (employeeInfoDiv) => {
-    employeeInfoDiv.remove()
-    overlay.classList.add("hidden")
+function displayModal(index)  {
+
+  let { name, dob, phone, email, location: { city, street, state, postcode,}, picture} = employees[index];
+
+  let date = new Date(dob.date);
+
+  const modalHTML = `
+    <img class="avatar" src="${picture.large}" />
+    <div class="text-container" >
+      <h2 class="name">${name.first} ${name.last}</h2>
+      <p class="email">${email}</p>
+      <p class="address">${city}</p>
+      <hr />
+      <p>${phone}</p>
+      <p class="address">${street}, ${state}, ${postcode}</p>
+      <p>Birthday:${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    </div> 
+  `;
+  overlay.classList.remove("hidden");
+  modalContainer.innerHTML = modalHTML;
 }
 
+gridContainer.addEventListener('click', event => {
 
+  if (event.target !== gridContainer) {
+
+    const card = event.target.closest(".card");
+    const index = card.getAttribute('data-index');
+
+    displayModal(index);
+  }
+});
+
+modalClose.addEventListener('click', () => {
+  overlay.classList.add("hidden");
+});
 
 
 
